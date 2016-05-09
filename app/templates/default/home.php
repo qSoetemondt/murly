6af34c018853732f4e439e9a1f2380a10fc458ef
Quentin Soetemondt <Quentin Soetemondt>
2016-05-09 12:20:30 +0200

    mulry

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -1,7 +1,7 @@
-<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+<?php $this->layout('layout', ['title' => 'Murly']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Let's code.</h2>
-	<p>Vous avez atteint la page d'accueil. Bravo.</p>
-	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
+	<h2>Murlyfication</h2>
+	<p>Venez Murlyfier vos URL!</p>
+	
 <?php $this->stop('main_content') ?>
