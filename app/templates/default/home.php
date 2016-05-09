fb93e029638f541ae6691af35a6ded287af9f9c8
Quentin Soetemondt <Quentin Soetemondt>
2016-05-09 13:44:18 +0200

    ajout form

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -4,4 +4,9 @@
 	<h2>Murlyfication</h2>
 	<p>Venez Murlyfier vos URL!</p>
 	
+	<form action="POST" id="input-murly">
+		<input class="form-control input-lg"  type="text" placeholder="coller votre URL">
+		<button class="btn btn-default" id="btn-murly" type="submit">Murlyfication</button>
+	</form>
+	
 <?php $this->stop('main_content') ?>
